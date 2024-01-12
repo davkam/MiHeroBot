34620c46c4606d1169fd1678a48ab1e5f2476bf4
davkam <david.jr.kamran@gmail.com>
2024-01-12 15:39:27 +0100

    Error-Fix Logger FileNotFound

--- logger/logger.py
+++ logger/logger.py
@@ -1,4 +1,9 @@
 import logging
+import os.path
+
+bot_log_file = "logger/logs/bot_info.log"
+data_log_file = "logger/logs/data_info.log"
+game_log_file = "logger/logs/game_info.log"
 
 class Logger():
     bot: logging.Logger = None
@@ -26,6 +31,26 @@ class Logger():
 
     @classmethod
     def set_loggers(cls):
-        cls.bot = cls.get_logger(name="discord", file_path="logger/logs/bot_info.log", level=logging.INFO)
-        cls.data = cls.get_logger(name="data", file_path="logger/logs/data_info.log", level=logging.INFO)
-        cls.game = cls.get_logger(name="game", file_path="logger/logs/game_info.log", level=logging.INFO)
+        if not os.path.exists(path=bot_log_file):
+            with open(bot_log_file, 'w') as file:
+                file.write('')
+
+            print(f'ERROR      - log             : File not found "{bot_log_file}", new file created!')
+
+        if not os.path.exists(path=data_log_file):
+            with open(data_log_file, 'w') as file:
+                file.write('')
+
+            print(f'ERROR      - log             : File not found "{data_log_file}", new file created!')
+
+        if not os.path.exists(path=game_log_file):
+            with open(game_log_file, 'w') as file:
+                file.write('')
+
+            print(f'ERROR      - log             : File not found "{game_log_file}", new file created!')
+
+        cls.bot = cls.get_logger(name="discord", file_path=bot_log_file, level=logging.INFO)
+        cls.data = cls.get_logger(name="data", file_path=data_log_file, level=logging.INFO)
+        cls.game = cls.get_logger(name="game", file_path=game_log_file, level=logging.INFO)
+
+        print(f'INFO       - log             : Loggers successfully initialized!')
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from bot.client import Client
 from logger.logger import Logger
 
 def main():
+    print("\n[SETTING LOGGERS...]")
     Logger.set_loggers()
 
-    print("[ESTABLISHING BOT CONNECTION...]")
+    print("\n[ESTABLISHING BOT CONNECTION...]")
     bot_client = Client()
     bot_client.run()
 
